81cec4aae1922537ffa6e0edca849f6acc655e15
Ryan McAlpin <rhmcalpin@gmail.com>
2018-12-23 18:49:22 -0800

    add nav from menu to new player

--- app/src/main/java/com/example/rygn/basketballrpg/MenuActivity.java
+++ app/src/main/java/com/example/rygn/basketballrpg/MenuActivity.java
@@ -1,7 +1,14 @@
 package com.example.rygn.basketballrpg;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +16,15 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        final Intent intent = new Intent(MenuActivity.this, NewPlayerActivity.class);
+
+        Button newGameButton = (Button) findViewById(R.id.new_game_button);
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MenuActivity.this.startActivity(intent);
+            }
+        });
     }
 }
